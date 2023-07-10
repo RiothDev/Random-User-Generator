246d01c616a05587977b277983a24e8098ebfe7f
HazuDev <justanotheralt102@gmail.com>
2023-07-09 19:44:52 -0600

    first

---
+++ Random Data/API.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace App
+{
+    internal class UserAPI
+    {
+        public static async Task<string> GetUser()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync("https://random-data-api.com/api/v2/users");
+
+                    if(response.IsSuccessStatusCode)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("> Request sent successfully...");
+                        Console.ResetColor();
+
+                        string json = await response.Content.ReadAsStringAsync();
+                        return json;
+                    }
+
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("> Status code: " + response.StatusCode);
+                    Console.ResetColor();
+
+                    return "";
+
+                } catch(Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("> Error: " + ex.Message);
+                    Console.ResetColor();
+
+                    return "";
+                }
+            }
+        }
+    }
+}
---
+++ Random Data/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace App
+{
+    public class Program
+    {
+        static void Print(string message, string content)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write($"> {message}: ");
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write(content + "\n");
+
+            Console.ResetColor();
+        }
+
+        static async Task _Init()
+        {
+            Console.Clear();
+            string json = await UserAPI.GetUser();
+
+            if (!string.IsNullOrEmpty(json))
+            {
+                JsonDocument jsonDoc = JsonDocument.Parse(json);
+                Random random = new Random();
+
+                string[] sexs = { "Female", "Male" };
+
+                Print("First name", jsonDoc.RootElement.GetProperty("first_name").GetString());
+                Print("Last name", jsonDoc.RootElement.GetProperty("last_name").GetString());
+                Print("Sex", sexs[random.Next(0, 2)]);
+                Print("Email", jsonDoc.RootElement.GetProperty("email").GetString());
+                Print("Phone number", jsonDoc.RootElement.GetProperty("phone_number").GetString());
+                Print("Username", jsonDoc.RootElement.GetProperty("username").GetString());
+                Print("Password", jsonDoc.RootElement.GetProperty("password").GetString());
+
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                Console.Write("> Continue? (Y/N): ");
+
+                ConsoleKeyInfo key = Console.ReadKey();
+                char opt = key.KeyChar;
+
+                if (char.ToLower(opt) == 'y')
+                {
+                    await _Init();
+                }
+            }
+        }
+
+        static async Task Main(string[] args)
+        {
+            await _Init();
+        }
+    }
+}
